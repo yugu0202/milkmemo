895bf2c41de1907e0d99739357302434ed524f47
yugu0202 <applerin1119@gmail.com>
2024-11-16 17:10:44 +0900

    firebaseとの連携設定

--- jmlog/lib/main.dart
+++ jmlog/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:jmlog/presentation/views/app.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
