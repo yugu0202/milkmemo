afc36811ccfac782b8a8526f01ee2410ae866a0e
yugu0202 <applerin1119@gmail.com>
2024-11-22 00:13:49 +0900

    使っていないライブラリをコメントアウト

--- jmlog/lib/main.dart
+++ jmlog/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jmlog/presentation/views/app.dart';
 
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
